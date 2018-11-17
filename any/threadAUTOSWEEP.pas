7f44b6e732cf31cc2e315277c9f6137064e45b2f
mirmalis <sarunas1991@gmail.com>
2018-11-17 13:06:27 +0200

    Added unit threadAUTOSWEEP

---
+++ any/threadAUTOSWEEP.pas
@@ -0,0 +1,51 @@
+unit sdpTHREADS;
+interface
+  var
+    spoil_ignoredMobsIDs : Array of Integer;
+    spoil_staticIgnoredMobsIDs: Array of Integer;
+    spoil_midPriorityMobsIDs: Array of Integer;
+    spoil_sweepID: Integer;
+    spoil_pause_interface: Boolean;
+    spoil_above_hp: Integer;
+  procedure auto_sweep();
+implementation
+  uses sdpSTRINGS;
+  procedure auto_sweep();
+    var 
+      aMob:TL2Npc; aSkill:TL2Skill; i:Integer;
+    begin
+      while true do
+      begin
+        for i := 0 to NPCList.Count - 1 do
+        begin
+          if (User.Hp < spoil_above_hp) then break;
+          aMob := NPCList.Items(i);
+          if (User.DistTo(aMob) < 300) then
+          begin
+            if   aMob.Valid 
+             and aMob.Dead 
+             and aMob.Sweepable 
+             and not is_in(aMob.ID, spoil_ignoredMobsIDs)
+             and not is_in(aMob.ID, spoil_staticIgnoredMobsIDs)
+             and (SkillList.ByID(spoil_sweepID,aSkill))
+            then
+            begin
+              if (spoil_pause_interface) then Engine.FaceControl(0, False);
+              Engine.SetTarget(aMob); Delay(250);
+              if (aSkill.EndTime < 1000) then Delay(aSkill.EndTime + 10);
+              Engine.UseSkill(aSkill);
+            end;
+          end;
+        end;
+        if (spoil_pause_interface) then Engine.FaceControl(0, True);
+        Delay(100);
+      end;
+    end;
+initialization
+  spoil_ignoredMobsIDs := [0];
+  spoil_staticIgnoredMobsIDs := [21411, 21408, 21407, 20824, 20820, 20818, 20982, 20981, 22106,22111, 22118];
+  spoil_midPriorityMobsIDs := [0];
+  spoil_sweepID := 42;
+  spoil_pause_interface := True;
+  spoil_above_hp := 70;
+end.
